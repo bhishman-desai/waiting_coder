6f662672f7a8917e24b43e51bd2c925afff14dc7
Bhishman <bh352449@dal.ca>
2024-06-23 16:15:30 -0300

    fix: fetching db config from environment variables

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,9 @@ const app = express();
 app.use(cors());
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   database: "waiting_coder"
 });
   
